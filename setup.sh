dd8fb59aff89ab3a85105a86296495435bb95df7
Adam Braman <glyoko1@gmail.com>
2025-07-23 14:50:06 -0700

    Create setup.sh, add install for git-diffn.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# Taken from https://github.com/ElectricRCAircraftGuy/eRCaGuy_dotfiles/blob/master/useful_scripts/README_git-diffn.md#33-installation
+cd ~/.bin
+wget https://raw.githubusercontent.com/ElectricRCAircraftGuy/eRCaGuy_dotfiles/master/useful_scripts/git-diffn.sh
+chmod +x git-diffn.sh
+mv git-diffn.sh git-diffn
